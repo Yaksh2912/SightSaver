fe6dcb54819f9ec8948961c9088346a4dd0858ff
Yaksh <yaksh2912@gmail.com>
2025-07-02 23:15:31 +0530

    hero tweaks

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
-import Face from '../assets/images/half_face.png';
+import React, { useEffect, useState } from 'react';
+import Face1 from '../assets/images/half_face.png';
+import Face2 from '../assets/images/sight-saver-logo.png';
+import Face3 from '../assets/images/features_image.png';
 import '../assets/styles/Hero.css';
 
+const images = [Face1, Face2, Face3];
+
 const Hero = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [fadeIn, setFadeIn] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFadeIn(false); // Start fade-out
+
+      setTimeout(() => {
+        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+        setFadeIn(true); // Trigger fade-in for new image
+      }, 400); // Match fade-out duration
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <section className="hero">
       <h1 className="hero-title">
@@ -11,7 +31,11 @@ const Hero = () => {
       <p className="hero-subtitle">
         Detection of chronic illness using Data Analytics for detection of Chronic Illness from Retinal Images.
       </p>
-      <img src={Face} alt="Eye Checkup" className="hero-image" />
+      <img
+        src={images[currentImageIndex]}
+        alt="AI Eye"
+        className={`hero-image ${fadeIn ? 'fade-in' : 'fade-out'}`}
+      />
     </section>
   );
 };
